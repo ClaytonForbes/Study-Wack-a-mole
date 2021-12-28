834cc8c18c48006d9d85f9d57906548d9ef078cb
ClaytonForbes <55164954+ClaytonForbes@users.noreply.github.com>
2021-12-27 19:36:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-// we start by collecting all the divs called square from the html and  named square for our JS, using :
-const square = document.querySelectorAll('.square')// learn what is querySelectorALl
+/// we start by collecting all the divs called square from the html and  named square for our JS, using :
+const squares = document.querySelectorAll('.square')// learn what is querySelectorALl
 //then we do the same for the mole 
-const mole = document.querySelectorAll('.mole')
+const mole = document.querySelector('.mole')
 // AND FOR TIME LEFT USING querySelector we use # because it is an Id  to keep track of the id
 const timeLeft = document.querySelector('#time-left')
 //  we use let for the score because this varibale will change
-let score = document .querySelector('#score')
+const score = document.querySelector('#score')
 
 //Creating the function for the game
-let result = 0 
-let currentTime = timeLeft.textContent
+let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
 
 // we will start by removing any class from divs to do this :
 // we need to creat e a function 
 //Step one is to make a fucntion that randomly slectors a square on the grid
-function randomSquare(){// start by removing any class from divs
+// start by removing any class from divs
     //using an arrow function and a foreach we romove class name for all of our square on the grid
     // and arrow function is another way to make a function. 
     // instead of using the key word function I used the => to create a function instead of 
@@ -24,42 +26,45 @@ function randomSquare(){// start by removing any class from divs
     
     }
     */
-    square.forEach(className => {
-        className.classList.remove('mole')
-    })
+    function randomSquare() {
+        squares.forEach(square => {
+          square.classList.remove('mole')
+        })
     //now we use math random to define a randomw postion on the grid
-    let randomPosition = square[Math.floor(Math.random()*9)]// we use math floor to round to the nearest whole number so that it is already equal to 9
-    randomPosition.classList.add('mole')
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
 
     //assign the id of the randomPosition to hitPostion for us to use later hit postion for us to use later
-     hitPosition = randomPosition.id
+    hitPosition = randomSquare.id
 
 
 }
-square.forEach(id => {
-    id.addEventListener('mouseup', () =>{
-        if(id.id === hitPosition){
-            result = result + 1
-            score.textContent = result
-        }
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+      if (square.id == hitPosition) {
+        result++
+        score.textContent = result
+        hitPosition = null
+      }
     })
-})
-
-function moveMole(){
-    let timerId = null
-    timerId = setInterval(randomSquare, 1000)
-}
-
- moveMole()
-
- function countDown() {
-    currentTime--
-    timeLeft.textContent = currentTime
-   
-    if (currentTime == 0) {
-      clearInterval(countDownTimerId)
-      clearInterval(timerId)
-      alert('GAME OVER! Your final score is ' + result)
-    }
- }
- let countDownTimerId = setInterval(countDown, 1000)
+  })
+  
+  function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+  }
+  
+  moveMole()
+  
+  function countDown() {
+   currentTime--
+   timeLeft.textContent = currentTime
+  
+   if (currentTime == 0) {
+     clearInterval(countDownTimerId)
+     clearInterval(timerId)
+     alert('GAME OVER! Your final score is ' + result)
+   }
+  
+  }
+  
+  let countDownTimerId = setInterval(countDown, 1000)
